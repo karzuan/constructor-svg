f270c3d51e7e4287a8bdaa7d639c5a5b450db621
karzuan <karzuan@gmail.com>
2020-06-29 12:06:13 +0800

    improved UI

--- src/colorpart.js
+++ src/colorpart.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './style.css';
+//import x from './images/x.svg';
+import xb from './images/x-bordered.svg';
 import { CirclePicker } from 'react-color';
 
 class Colorpart extends React.Component {
@@ -26,24 +28,30 @@ class Colorpart extends React.Component {
         return (
             <React.Fragment>
                 <h5>Change color</h5>
-                <ul>
-                    <li ><p className="objectPart" onClick={() => this.handleClick('edge')} >Edge</p></li>
-                    {/*
+                <div className='row'>
+                    <div className="col-8">
+                        <ul>
+                            <li ><p className="objectPart" onClick={() => this.handleClick('edge')} >Edge</p></li>
+                                {/*
                     add a state for each part open/closed and based on that I will render or not CirclePicker
                     { isTrue && <Component /> }
                     */}
-                    {this.state.activedge && <div className="form-inline"><CirclePicker
-                        color={this.props.edge}
-                        onChangeComplete={this.props.handleEdgeColorChange}
-                    /><span className="close" onClick={() => this.handleClick('edge')} > X </span></div>}
+                        {this.state.activedge && <div className="form-inline box"><CirclePicker
+                            color={this.props.edge}
+                            onChangeComplete={this.props.handleEdgeColorChange}
+                        /><span className="close" onClick={() => this.handleClick('edge')} > <img src={xb} alt="close" /> </span></div>}
+                            <li><p className="objectPart" onClick={() => this.handleClick('fill')}>Fill</p></li>
+                            {this.state.activfill && <div className="form-inline box"><CirclePicker
+                            color={this.props.fill}
+                            onChangeComplete={this.props.handleFillColorChange}
+                        /><span className="close" onClick={() => this.handleClick('fill')} > <img src={xb} alt="close" /></span></div>}
+                        </ul>
+                    </div>
+                    <div className="col-4">
+                    
 
-                    <li><p className="objectPart" onClick={() => this.handleClick('fill')}>Fill</p></li>
-                    {this.state.activfill && <div className="form-inline"><CirclePicker
-                        color={this.props.fill}
-                        onChangeComplete={this.props.handleFillColorChange}
-                    /><span className="close" onClick={() => this.handleClick('fill')} > X </span></div>}
-                </ul>
-
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
